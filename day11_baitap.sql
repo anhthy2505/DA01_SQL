4c621f834bd6b29e5f0b1e7d860872687ababc76
anhthy2505 <155831716+anhthy2505@users.noreply.github.com>
2024-01-17 21:40:57 +0700

    Update day11_baitap.sql

--- day11_baitap.sql
+++ day11_baitap.sql
@@ -61,4 +61,20 @@ on p.page_id = k.page_id
 where k.page_id is null
 
 
--- bài giữa kì
+-- BÀI GIỮA KÌ 
+-- câu hỏi 1
+select distinct replacement_cost from film
+order by replacement_cost
+limit 1
+
+-- câu hỏi 2
+select 
+case 
+when replacement_cost between 9.99 and 19.99 then 'low'
+when replacement_cost between 20.00 and 24.99 then 'medium'
+when replacement_cost between 25.00 and 29.99 then 'high'
+end as category
+from film
+group by category
+having category = 'low'
+count(film_id) as low_count
