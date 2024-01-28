43dd0352b9622dc116469963a7a32d8ad1c0086e
anhthy2505 <155831716+anhthy2505@users.noreply.github.com>
2024-01-28 18:25:29 +0700

    Update day13_baitap.sql

--- day13_baitap.sql
+++ day13_baitap.sql
@@ -58,3 +58,53 @@ FROM Sales as s
 INNER JOIN (SELECT product_id, MIN(year) as min_year FROM Sales GROUP BY product_id) as first_year 
 ON s.product_id = first_year.product_id 
 AND s.year = first_year.min_year;
+
+--ex8:
+SELECT c.customer_id
+FROM Customer c
+JOIN Product p ON c.product_key = p.product_key
+GROUP BY c.customer_id
+HAVING COUNT(DISTINCT c.product_key) = (SELECT COUNT(*) FROM Product);
+
+--ex9:
+SELECT e.employee_id FROM Employees e
+LEFT JOIN Employees m ON e.manager_id = m.employee_id
+WHERE e.salary < 30000 AND m.employee_id IS NULL
+ORDER BY e.employee_id;
+
+--ex10:
+SELECT COUNT(DISTINCT company_id) AS duplicate_companies
+FROM (SELECT company_id, title, description FROM job_listings
+GROUP BY company_id, title, description
+HAVING COUNT(job_id) > 1) AS duplicates;
+
+--ex11:
+SELECT u.name AS results
+FROM Users u
+JOIN (SELECT user_id FROM MovieRating
+GROUP BY user_id
+ORDER BY COUNT(*) DESC, user_id
+LIMIT 1) AS top_user 
+ON u.user_id = top_user.user_id
+
+UNION ALL
+SELECT m.title AS results
+FROM Movies m
+JOIN (SELECT movie_id FROM MovieRating
+WHERE MONTH(created_at) = 2 AND YEAR(created_at) = 2020
+GROUP BY movie_id
+ORDER BY AVG(rating) DESC, movie_id
+LIMIT 1) AS top_movie 
+ON m.movie_id = top_movie.movie_id;
+
+--ex12: 
+WITH AllFriendships as (SELECT requester_id as id FROM RequestAccepted
+UNION ALL
+SELECT accepter_id as id FROM RequestAccepted),
+FriendCounts as (SELECT id, COUNT(*) as num FROM AllFriendships
+GROUP BY id)
+SELECT fc.id, fc.num
+FROM FriendCounts as fc
+ORDER BY fc.num DESC, fc.id
+LIMIT 1;
+
